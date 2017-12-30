f0e76161523ac4fb16e10cd0e97370a1b7fd12b1
Istvan <istvan_szasz@hotmail.com>
2017-12-30 14:41:16 +0100

    added CORS support with jQuery

--- data.service.js
+++ data.service.js
@@ -3,15 +3,26 @@ angular.module('G5Data').service('DataService', function() {
 
     var vm = this;
 
+    jQuery.ajaxPrefilter(function(options) {
+        if (options.crossDomain && jQuery.support.cors) {
+            options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+            console.log(options.url);
+        }
+    });
+
     vm.getData = function (http, url){
-        return http({
-        method: 'GET',
-        headers: {
-            'Access-Control-Allow-Origin' : '*',
-            'Access-Control-Allow-Headers' : 'Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With',
-            'Access-Control-Allow-Methods' : 'GET, PUT, POST',
-        },
-        url: url
-        });
+        // return http({
+        // method: 'GET',
+        // headers: {
+        // 'Access-Control-Allow-Origin': '*',
+        // 'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+        // 'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token'
+        // },
+        // url: url
+        // });
+
+        return $.ajax({
+            url: url,
+          });
     }
 });
